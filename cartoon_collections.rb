e2c04f7863454694cad15318d944982ff19a72c9
Panela Leung <leungpanela@gmail.com>
2019-08-26 16:45:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,8 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if cheese_types.each {|i| i.include? (array)}
-  return i
+  if cheese_types.include? (array)
+  return array
+end
 
 end
